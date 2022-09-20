bfa85e2d5c6c66b7b8cfee1421b9f004af48b2f4
Simon Biddle <simon.biddle@dbmvircon.com>
2022-09-20 20:29:26 +0800

    take 4

--- middleware.js
+++ middleware.js
@@ -12,7 +12,7 @@ export async function middleware(req) {
     });
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("http://localhost:3000/home");
+    if (!session) return NextResponse.redirect("/home");
     // If user is authenticated, continue.
   }
 }
